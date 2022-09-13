c02f5aed9c2ff1ef6060841739c448a6474888dc
Girl Icon Shanti <91754626+Shanti-kumari9@users.noreply.github.com>
2022-09-13 23:34:16 +0530

    Add files via upload

---
+++ sdfgh.py
@@ -0,0 +1,66 @@
+# print("wellcome to sbi bank")
+# print("selact your language")
+# print("please insert your card")
+# language=input("enter the languege : ")
+# if language=="english":
+#     password=int(input("enter the password"))
+#     if password==9999:
+#         print("transaction:1.withdraw 2.balanceenquary 3.pingenaretion 4.deposite 5.exsist")
+#         transaction=int(input("enter the trnsaction"))
+#         amount=30000
+#         if transaction==1:
+#             withdraw=int(input("enter the withdraw"))
+#             if withdraw<=amount:
+#                 print("collect your cash",withdraw)
+#                 print("remaing amount",amount-withdraw)
+#             else:
+#                 print("not withdraw")
+#         elif transaction==2:
+#             print(amount)
+#         elif transaction==3:
+#              pingenaretion=int(input("enter the pingenaration"))
+#              if pingenaretion==pingenaretion:
+#                 print("new pingenaretion")
+#              else:
+#                 print("not correct")
+#         elif transaction==4:
+#              money=int(input("enter the money"))
+#              if money+ amount:
+#                 print("total balance is",amount+money)
+#         elif transaction==5:
+#              exsit=input("enter the exsit ")
+#              if exsit=="yes":
+#                 print("thank you for visiting")
+#              else:
+#                 print("choose your transaction")
+#         else:
+#             print("choose correct value")
+#     else:
+#         print("enter the correct password")
+# else:
+#     print("does not language")
+
+
+
+# # i = 10
+# # if i>15:
+# #     print('10 is less than 15')
+# # print('I am Not in if')
+
+
+a=int(input("enter any number"))
+b=int(input("enter any number"))
+c=int(input("enter any number"))
+if a<b and a<c and b>a and b>c:
+    print("a is max and b is min")
+elif b>a and b>c and a<b and a<c:
+    print(" b is max and c is min")
+else:
+    print("c is max and a is min")
+
+
+
+a=int(input("enter any number"))
+
+
+
